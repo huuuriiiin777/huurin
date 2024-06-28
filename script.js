2abbc1c003e5ca83ba62e631dde9758a68317e20
huuuriiiin777 <roooon1919@gmail.com>
2024-06-28 17:38:38 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = '1256165703497809970';
 const REDIRECT_URI = 'https://huuuriiiin777.github.io/huurin/callback.html';
-const DISCORD_ENDPOINT = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=token&scope=identify%20guilds.join`;
+const DISCORD_ENDPOINT = `https://discord.com/oauth2/authorize?client_id=1256165703497809970&response_type=code&redirect_uri=https%3A%2F%2Fhuuuriiiin777.github.io%2Fhuurin%2Fcallback.html&scope=guilds.join`;
 
 document.getElementById('auth-button').addEventListener('click', () => {
     window.location.href = DISCORD_ENDPOINT;
