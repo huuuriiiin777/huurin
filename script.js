c62898403f05e41736fd5d8397707246ffa2240e
huuuriiiin777 <roooon1919@gmail.com>
2024-06-28 17:35:46 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const CLIENT_ID = 'YOUR_CLIENT_ID';
-const REDIRECT_URI = 'https://yourusername.github.io/your-repo-name/callback.html';
+const CLIENT_ID = '1256165703497809970';
+const REDIRECT_URI = 'https://huuuriiiin777.github.io/huurin/callback.html';
 const DISCORD_ENDPOINT = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&response_type=token&scope=identify%20guilds.join`;
 
 document.getElementById('auth-button').addEventListener('click', () => {
